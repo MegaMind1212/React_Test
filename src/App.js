09ad02d92064e328a0ba1d099aecae4a5f8d6b1a
MegaMind1212 <borkaryash12@gmail.com>
2024-03-06 22:34:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="logo" />
+        <p>
+          Happy birthday koli, from the <span className="red-text">supreme</span> borkar
+        </p>
+        <a
+          className="App-link"
+          href="https://yashs-portfolio.my.canva.site/yashs-note"
+          rel="noopener noreferrer"
+        >
+          Read Me
+        </a>
+        <p className="spaced-text">
+        {'   '}If you're partying! fyi you might need abundant time to read this{'   '} 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
